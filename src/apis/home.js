4ec97fee80f4a550266fe3d15a2be918226f20b5
nico <1256346881@qq.com>
2024-01-02 19:51:12 +0800

    home-product图片渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -36,3 +36,15 @@ export const getHotAPI = () => {
     })
 }
 // return httpInstance('home/hot', 'get', {})
+
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+    return httpInstance({
+        url: '/home/goods'
+    })
+}
