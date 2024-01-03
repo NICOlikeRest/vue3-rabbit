af305e6d5aae7912101ea4c8ae0f6beef3f1ac66
nico <1256346881@qq.com>
2024-01-03 14:57:10 +0800

    无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -33,7 +33,27 @@ const getGoodList = async () => {
 onMounted(() => getGoodList())
 
 
+const tabChange = () => {
+    // console.log(11);
+    reqData.value.page = 1
+    getGoodList()
 
+}
+
+
+const load = async () => {
+    // console.log(11);
+    reqData.value.page ++
+    // console.log(reqData.value.page);
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = [...goodList.value,...res.result.items]
+    //加载完毕
+    if (res.result.items.length === 0) {
+        disabled.value = true
+    }
+}
+
+const disabled = ref(false)
 
 
 </script>
@@ -50,12 +70,12 @@ onMounted(() => getGoodList())
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
 
