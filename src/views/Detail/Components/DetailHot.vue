a9251954b820f93f23a7b98852cd9eab94d6badf
nico <1256346881@qq.com>
2024-01-03 19:21:34 +0800

    详情热榜里props-title

--- src/views/Detail/Components/DetailHot.vue
+++ src/views/Detail/Components/DetailHot.vue
@@ -1,7 +1,21 @@
 <script setup>
 import { fetchHotGoodsAPI } from '@/apis/detail.js'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+
+const props = defineProps({
+   hotType: {
+    type: Number
+   } 
+})
+
+const TYPEMAP = {
+    1: '24小时热榜',
+    2: '周热榜'
+}
+
+const title = computed(() => TYPEMAP[props.hotType])
+
 const route = useRoute()
 
 const hotList = ref([])
@@ -21,7 +35,7 @@ onMounted(() => getHotList())
 
 <template>
     <div class="goods-hot">
-        <h3>周日榜单</h3>
+        <h3>{{title}}</h3>
         <!-- 商品区块 -->
         <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
             <img :src="item.picture" alt="" />
